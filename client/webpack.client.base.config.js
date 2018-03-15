7c9539d5d13c62cd37bb72b13fb027cdaa662786
RyanNovas <ryan@quill.org>
2018-03-15 16:07:40 -0400

    adds uglifier/minifier when in prod

--- client/webpack.client.base.config.js
+++ client/webpack.client.base.config.js
@@ -2,17 +2,62 @@ const webpack = require('webpack');
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 const devBuild = process.env.NODE_ENV !== 'production';
 const firebaseApiKey = process.env.FIREBASE_API_KEY;
 const firebaseDatabaseUrl = process.env.FIREBASE_DATABASE_URL;
 const pusherKey = process.env.PUSHER_KEY;
 const defaultUrl = process.env.DEFAULT_URL;
 const cdnUrl = process.env.CDN_URL;
-const { resolve } = require('path');
+const { resolve, } = require('path');
 const webpackConfigLoader = require('react-on-rails/webpackConfigLoader');
+
 const configPath = resolve('..', 'config');
-const { output } = webpackConfigLoader(configPath);
+const { output, } = webpackConfigLoader(configPath);
 const nodeEnv = devBuild ? 'development' : 'production';
+
+const devPlugins = [new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify(nodeEnv),
+    FIREBASE_API_KEY: JSON.stringify(firebaseApiKey),
+    FIREBASE_DATABASE_URL: JSON.stringify(firebaseDatabaseUrl),
+    PUSHER_KEY: JSON.stringify(pusherKey),
+    DEFAULT_URL: JSON.stringify(defaultUrl),
+    CDN_URL: JSON.stringify(cdnUrl),
+  },
+  TRACE_TURBOLINKS: devBuild,
+}),
+  new webpack.LoaderOptionsPlugin({
+    test: /\.scss$/,
+    options: {
+      sassResources: [
+        './app/assets/styles/app-variables.scss'
+      ],
+    },
+  }),
+  new webpack.LoaderOptionsPlugin({
+    test: /\.s?css$/,
+    options: {
+      postcss: [autoprefixer],
+    },
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor-bundle.js',
+    minChunks: Infinity,
+  }),
+  new ManifestPlugin({
+    publicPath: output.publicPath,
+    writeToFileEmit: true,
+  })];
+
+const plugins = () => {
+  if (devBuild) {
+    return devPlugins;
+  }
+  return devPlugins.splice(1, 0, new webpack.optimize.UglifyJsPlugin());
+};
 
 module.exports = {
   context: __dirname,
@@ -54,7 +99,7 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     modules: [
       './node_modules',
-      './app',
+      './app'
     ],
     alias: {
       lib: path.join(process.cwd(), 'app', 'lib'),
@@ -62,42 +107,7 @@ module.exports = {
       'react-dom': path.resolve('./node_modules/react-dom'),
     },
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(nodeEnv),
-        FIREBASE_API_KEY: JSON.stringify(firebaseApiKey),
-        FIREBASE_DATABASE_URL: JSON.stringify(firebaseDatabaseUrl),
-        PUSHER_KEY: JSON.stringify(pusherKey),
-        DEFAULT_URL: JSON.stringify(defaultUrl),
-        CDN_URL: JSON.stringify(cdnUrl),
-      },
-      TRACE_TURBOLINKS: devBuild,
-    }),
-    new webpack.LoaderOptionsPlugin({
-      test: /\.scss$/,
-      options: {
-        sassResources: [
-          './app/assets/styles/app-variables.scss',
-        ]
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      test: /\.s?css$/,
-      options: {
-        postcss: [autoprefixer]
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor-bundle.js',
-      minChunks: Infinity,
-    }),
-    new ManifestPlugin({
-      publicPath: output.publicPath,
-      writeToFileEmit: true
-    }),
-  ],
+  plugins: plugins(),
   module: {
     rules: [
       {
@@ -129,13 +139,13 @@ module.exports = {
         use: [
           {
             loader: 'expose-loader',
-            query: 'jQuery'
+            query: 'jQuery',
           },
           {
             loader: 'expose-loader',
-            query: '$'
+            query: '$',
           }
-        ]
+        ],
       },
       {
         test: /\.json$/,
@@ -143,7 +153,7 @@ module.exports = {
           {
             loader: 'json-loader',
           }
-        ]
+        ],
       }
     ],
   },
